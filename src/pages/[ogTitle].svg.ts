67740518026d6b81ae5caabfd9714f30d99bd729
Filip Kaštovský <devfilipkastovsky@gmail.com>
2023-07-20 14:10:17 +0200

    fix: fucking regex

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -13,6 +13,6 @@ export const getStaticPaths = () => {
   return posts
     .filter(({ data }) => !data.ogImage)
     .map(({ data }) => ({
-      params: { ogTitle: data.title.replace(/\?\&\./g, "") },
+      params: { ogTitle: data.title.replace(/\?|\&|\:/g, "") },
     }));
 };
